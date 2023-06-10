4093a69a4e8c3cd46825c2d7788ea96a574c8c16
Suchat Tangjarukij <125326168+RadstalST@users.noreply.github.com>
2023-06-10 19:56:45 +1000

    make about more playful

--- pages/About.py
+++ pages/About.py
@@ -1,10 +1,12 @@
 import streamlit as st
 import os
+from streamlit_lottie import st_lottie
+
 
 st.title('About')
 st.info('This is the about page for the product comparison app')
-st.header('Team Members')
 
+st.header('Team Members')
 team_cols = st.columns(3)
 with team_cols[0]:
     st.image(
@@ -30,7 +32,86 @@ with team_cols[2]:
     st.write('Creative/Developer')
     st.write('[LinkedIn](https://www.linkedin.com/in/chanawee/)')
 
+## Problem Statement
+st.markdown('''
+## Problem Statement
 
-# read /pages/src/about.md and print to screen
-with open(os.path.join(os.path.dirname(__file__), "src/about.md"), "r") as f:
-    st.markdown(f.read())
+In today's digital age, consumers face the daunting task of comparing numerous products before making a purchase decision. The overwhelming amount of information available online often leads to information overload, making it difficult for consumers to gather and analyze relevant data efficiently. As a result, they may struggle to make informed decisions and end up with products that don't meet their needs.
+''')
+
+
+## Solution Overview
+cols =  st.columns(2)
+with cols[0]:
+    st.markdown('''
+## Solution Overview
+To address this problem, we have developed an AI agent that leverages cutting-edge technologies, including Langchain and Streamlit, to revolutionize the product comparison process. Our AI agent acts as a virtual assistant, sifting through vast amounts of data from various sources and generating comprehensive and user-friendly comparison articles.
+    ''')
+
+with cols[1]:
+    st_lottie("https://assets2.lottiefiles.com/packages/lf20_cuKhxGQKFB.json")
+
+
+## Features
+st.markdown('''
+## Key Features
+''')
+            
+cols = st.columns(2)
+with cols[0]:
+    st.markdown('''
+### Natural Language Processing
+
+Our AI agent utilizes Langchain's advanced language processing capabilities to extract relevant information from product descriptions, reviews, and specifications. This ensures accurate and comprehensive data analysis.
+'''            
+    )
+
+with cols[1]:
+    st.markdown('''
+### Data Aggregation
+
+By aggregating data from multiple sources, our AI agent provides a holistic view of each product. This saves consumers valuable time and effort by eliminating the need to visit numerous websites and read through multiple reviews.
+''')
+
+cols = st.columns(2)
+with cols[0]:
+    st.markdown('''
+### Customizable Criteria
+
+We understand that every consumer has unique preferences and priorities. Our AI agent allows users to define their own criteria for comparison, ensuring that the generated articles align with their specific needs.
+'''
+    )
+
+with cols[1]:
+    st.markdown('''
+### Streamlined Interface
+
+Powered by Streamlit, our web app framework of choice, our AI agent offers a seamless and intuitive user experience. The interface is designed to be user-friendly and interactive, enabling consumers to navigate and explore comparison articles effortlessly.
+''')    
+
+## Roadmap
+st.markdown('''
+## Roadmap
+
+We have an exciting roadmap ahead, aiming to continuously enhance our AI agent's capabilities and expand its reach. Some of our planned future features include:
+''')     
+cols = st.columns(2)
+
+
+with cols[0]:
+    st_lottie("https://assets4.lottiefiles.com/packages/lf20_NIqx0dKgO5.json")
+with cols[1]:
+    st.markdown('''
+    1. Creation of AI agent to collect data from the internet and analyse.
+    2. Integration with additional e-commerce platforms, allowing users to directly compare products from their favorite online stores.
+    3. Sentiment analysis to provide users with insights into customer satisfaction and opinions.
+    4. Mobile app development to extend our AI agent's accessibility and convenience.
+    ''')
+            
+
+
+# load mardown file
+with open("./pages/src/about.md", "r") as f:
+    about_md = f.read()
+    st.markdown(about_md)
+
--- pages/src/about.md
+++ pages/src/about.md
@@ -1,23 +1,4 @@
 # About This Project
-
-## Problem Statement
-
-In today's digital age, consumers face the daunting task of comparing numerous products before making a purchase decision. The overwhelming amount of information available online often leads to information overload, making it difficult for consumers to gather and analyze relevant data efficiently. As a result, they may struggle to make informed decisions and end up with products that don't meet their needs.
-
-## Solution Overview
-
-To address this problem, we have developed an AI agent that leverages cutting-edge technologies, including Langchain and Streamlit, to revolutionize the product comparison process. Our AI agent acts as a virtual assistant, sifting through vast amounts of data from various sources and generating comprehensive and user-friendly comparison articles.
-
-## Key Features
-
-- **Natural Language Processing:** Our AI agent utilizes Langchain's advanced language processing capabilities to extract relevant information from product descriptions, reviews, and specifications. This ensures accurate and comprehensive data analysis.
-
-- **Data Aggregation:** By aggregating data from multiple sources, our AI agent provides a holistic view of each product. This saves consumers valuable time and effort by eliminating the need to visit numerous websites and read through multiple reviews.
-
-- **Customizable Criteria:** We understand that every consumer has unique preferences and priorities. Our AI agent allows users to define their own criteria for comparison, ensuring that the generated articles align with their specific needs.
-
-- **Streamlined Interface:** Powered by Streamlit, our web app framework of choice, our AI agent offers a seamless and intuitive user experience. The interface is designed to be user-friendly and interactive, enabling consumers to navigate and explore comparison articles effortlessly.
-
 ## Target Market
 
 Our AI agent is designed to cater to a wide range of consumers seeking efficient and reliable product comparison solutions. This includes tech-savvy individuals, online shoppers, and even businesses that require detailed product analysis for their procurement processes. With the growing e-commerce market and the increasing demand for informed decision-making, our target market is extensive and promising.
@@ -38,18 +19,6 @@ Our AI agent stands out from the competition in several ways:
 
 - **User-Friendly Interface:** Powered by Streamlit, our AI agent offers a visually appealing and easy-to-use interface, enhancing the user experience and encouraging engagement.
 
-## Future Roadmap
-
-We have an exciting roadmap ahead, aiming to continuously enhance our AI agent's capabilities and expand its reach. Some of our planned future features include:
-
-- Integration with additional e-commerce platforms, allowing users to directly compare products from their favorite online stores.
-
-- Sentiment analysis to provide users with insights into customer satisfaction and opinions.
-
-- Mobile app development to extend our AI agent's accessibility and convenience.
-
 ## Conclusion
 
 Our AI agent is poised to disrupt the product comparison landscape by offering a streamlined, user-friendly, and comprehensive solution. With our advanced technology and commitment to continuous improvement, we aim to empower consumers and businesses to make well-informed decisions confidently.
-
-For more information or to request a demo, please contact us at [email address] or visit our website at [website URL].
